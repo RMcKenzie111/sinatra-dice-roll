c44b2c9d5c6f93c8b37eda61b19ce67ce68ba770
Rebecca McKenzie <63881668+RMcKenzie111@users.noreply.github.com>
2024-03-04 22:28:03 +0000

    added layout file and yield

--- dice.rb
+++ dice.rb
@@ -36,7 +36,7 @@ get("/dice/2/6") do
   @outcome = "You rolled a #{first_die} and #{second_die} for a total of #{sum}"
 
 # Send back both numbers and the total in the body of the response
-  erb(:two_six)
+  erb(:two_six, { :layout => :wrapper })
 end
 
 get("/dice/2/10") do
@@ -47,7 +47,7 @@ get("/dice/2/10") do
   sum = first_die + second_die
     
   @outcome = "You rolled a #{first_die} and #{second_die} for a total of #{sum}"
-  erb(:two_ten)
+  erb(:two_ten, { :layout => :wrapper })
 end          
 
 get("/dice/1/20") do
@@ -57,7 +57,7 @@ get("/dice/1/20") do
   result = one_die
     
   @outcome = "You rolled a #{one_die}."
-  erb(:one_twenty)
+  erb(:one_twenty, { :layout => :wrapper })
 end          
 
 get("/dice/5/4") do
@@ -71,5 +71,5 @@ get("/dice/5/4") do
   sum = first_die + second_die + third_die + fourth_die + fifth_die
     
   @outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and a #{fifth_die} for a total of #{sum}"
-  erb(:five_four)
+  erb(:five_four, { :layout => :wrapper })
 end          
