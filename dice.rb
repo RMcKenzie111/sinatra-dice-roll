afb110f468544205e26e7a239d0eb6de80fcf34a
Rebecca McKenzie <63881668+RMcKenzie111@users.noreply.github.com>
2024-03-04 21:08:22 +0000

    adaded erb and views folder

--- dice.rb
+++ dice.rb
@@ -20,15 +20,9 @@ get("/giraffe") do
 end
 
 
-# In order to have " within our String, we had to escape them with a backslash. Then, Ruby knows not to terminate the String there, and instead treats the " as just another character to be included in the String.
+# In order to have " within our String, we had to escape them with a backslash. Then, Ruby knows not to terminate the String there, and instead treats the " as just another character to be included in the String. Using views is better
 get("/") do
-  "<h1>Dice Roll</h1>
-   <ul>
-    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
-    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
-    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
-    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
-   </ul> "
+  erb(:elephant)
 end
 
 
